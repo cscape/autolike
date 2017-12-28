c0ff8cd4ae515caf466d14da9e470053af1d3d16
Alex Solano <cioltdev@outlook.com>
2017-12-27 20:49:31 -0500

    Add support for auto-liking tweets

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Twitter = require('twitter');
 
+/**
+ * Tweet Auto-Liker
+ * @param {{consumerKey: string, consumerSecret: string, accessToken: string, accessTokenSecret: string}} credentials 
+ * @param {boolean} useStream 
+ * @param {string} feed 
+ * @param {{count: number, since_id: number, include_entities: boolean, follow: string, track: string}} opts
+ * Only the follow and track properties are needed for streams. They do not apply for non-streams. 
+ */
 module.exports = function (credentials, useStream, feed = 'statuses/home_timeline', opts) {
     const client = new Twitter({
-      consumer_key: process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      consumer_key: credentials.consumerKey,
+      consumer_secret: credentials.consumerSecret,
       access_token_key: credentials.accessToken,
       access_token_secret: credentials.accessTokenSecret
     });
@@ -12,34 +20,56 @@ module.exports = function (credentials, useStream, feed = 'statuses/home_timelin
     /** Check Twitter Timeline
      * @return {null} Doesn't return anything.
      */
-    function checkFeed() {
-      client.get(feed, (opts || {
-        count: 15, since_id: lastTweet, include_entities: false
+    let checkFeed = () => {
+      let screen_name = opts.screen_name;
+      client.get(feed, ({
+        screen_name, count: 20,
+        include_rts: opts.include_rts,
+        since_id: lastTweet, include_entities: false
       }), (error, tweets, response) => {
         if (!error) {
-          const tweet = tweets[0];
-          lastTweet = tweet.id_str;
-          if (tweet.user.id_str !== credentials.id) {
-            client.post('favorites/create', {
-              id: tweet.id_str, include_entities: false
-            }, (error, tweet, response) => {
-              if (error) console.error(error);
-            });
-          }
+          tweets.forEach(function(tweet, index){
+            lastTweet = tweet.id_str;
+            if (tweet.favorited === false && tweet.user.screen_name !== screen_name) {
+              client.post('favorites/create', {
+                id: tweet.id_str, include_entities: false
+              }, (error2, tweet2, response2) => {
+                if (error2) console.error(error2);
+                else console.log(`Liked ${tweet.id_str}`);
+              });
+            }
+          });
         } else {
           console.log(error);
         }
       });
-    }
-
+    };
+    
     if (useStream === false) {
-      const interval = setInterval(() => { checkFeed(); }, 600000); // every 6 minutes
+      const interval = setInterval(() => {
+        checkFeed();
+        console.log('Interval finished lap.')
+      }, 60000); // every 6 minutes
       checkFeed();
     } else if (useStream === true) {
-      client.stream('statuses/filter', ({
+      client.stream('statuses/filter', {
         stall_warnings: true, follow: opts.follow, track: opts.track
-      }, function (error, data) {
-        console.log(data);
-      });;
+      }, function (stream) {
+        stream.on('data', function(data) {
+          if (!data.friends && data.user && data.in_reply_to_screen_name === null && data && data.retweeted === false && data.user){
+            console.log(data);
+            client.post('favorites/create', {
+              id: data.id_str
+            }, (error, tweet, response) => {
+              if (error) console.error(error);
+              else console.log(`Liked ${data.id_str}`);
+            });
+          }
+        });
+       
+        stream.on('error', function(error) {
+          console.log(error);
+        });
+      });
     }
-  }
+  };
